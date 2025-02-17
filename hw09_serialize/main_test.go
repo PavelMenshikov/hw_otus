bef78752055117446fbae53c6708cbbd2774785d
PavelMenshikov <creepskip2@gmail.com>
2025-02-17 19:01:16 +0300

    Исправлены тесты

--- hw09_serialize/main_test.go
+++ hw09_serialize/main_test.go
@@ -8,7 +8,7 @@ import (
 
 // Тестируем JSON-сериализацию и десериализацию
 func TestJSONSerialization(t *testing.T) {
-	// Исходная книга
+
 	originalBook := Book{
 		ID:     1,
 		Title:  "Грокаем алгоритмы",
@@ -19,31 +19,26 @@ func TestJSONSerialization(t *testing.T) {
 		Sample: []byte("Это фрагмент книги."),
 	}
 
-	// Сериализация в JSON
 	jsonData, err := ToJSON(originalBook)
 	if err != nil {
 		t.Fatalf("Ошибка сериализации: %v", err)
 	}
 
-	// Десериализация из JSON
 	newBook, err := FromJSON(jsonData)
 	if err != nil {
 		t.Fatalf("Ошибка десериализации: %v", err)
 	}
 
-	// Сравниваем книги (используем reflect.DeepEqual для сравнения структур)
 	if !reflect.DeepEqual(originalBook, newBook) {
 		t.Errorf("Ожидали %+v, а получили %+v", originalBook, newBook)
 	}
 
-	// Проверяем, что JSON можно корректно распарсить в карту
 	var parsedData map[string]interface{}
 	err = json.Unmarshal(jsonData, &parsedData)
 	if err != nil {
 		t.Fatalf("Ошибка парсинга JSON: %v", err)
 	}
 
-	// Проверяем, что в JSON есть ожидаемые поля
 	expectedFields := []string{"id", "title", "author", "year", "size", "rate", "sample"}
 	for _, field := range expectedFields {
 		if _, ok := parsedData[field]; !ok {
