24d6accf3f204541be8193e98fe1ef79250196c3
PavelMenshikov <creepskip2@gmail.com>
2025-03-11 21:34:34 +0300

    тесты

---
+++ hw10_motion_sensor/main_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSensorDataGenerator(t *testing.T) {
+	dataChan := make(chan int)
+
+	go sensorDataGenerator(dataChan)
+
+	count := 0
+	for range dataChan {
+		count++
+	}
+
+	if count != 120 {
+		t.Errorf("Ожидалось 120 значений, получено %d", count)
+	}
+}
+
+func TestDataProcessor(t *testing.T) {
+	dataChan := make(chan int, 20)
+	processedChan := make(chan float64)
+
+	go dataProcessor(dataChan, processedChan)
+
+	expectedBatches := [][]int{
+		{10, 20, 30, 40, 50, 60, 70, 80, 90, 100},
+		{5, 15, 25, 35, 45, 55, 65, 75, 85, 95},
+	}
+
+	for _, batch := range expectedBatches {
+		sum := 0
+		for _, v := range batch {
+			dataChan <- v
+			sum += v
+		}
+	}
+
+	close(dataChan)
+
+	for _, batch := range expectedBatches {
+		sum := 0
+		for _, v := range batch {
+			sum += v
+		}
+		expectedAvg := float64(sum) / 10.0
+
+		actualAvg, open := <-processedChan
+		if !open {
+			t.Errorf("Канал processedChan закрылся раньше времени")
+			return
+		}
+
+		if actualAvg != expectedAvg {
+			t.Errorf("Ожидалось %.2f, получено %.2f", expectedAvg, actualAvg)
+		}
+	}
+
+	_, open := <-processedChan
+	if open {
+		t.Errorf("Канал processedChan должен быть закрыт")
+	}
+}
