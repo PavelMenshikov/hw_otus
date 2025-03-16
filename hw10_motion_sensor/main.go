24a7c674053479f48498f56c82914a96505d9e71
PavelMenshikov <creepskip2@gmail.com>
2025-03-16 18:53:48 +0300

    Таймер,буфер

--- hw10_motion_sensor/main.go
+++ hw10_motion_sensor/main.go
@@ -1,74 +1,73 @@
 package main
 
 import (
-	"crypto/rand"
-	"fmt"
-	"log"
-	"math/big"
-	"sync"
-	"time"
+  "fmt"
+  "math/rand"
+  "sync"
+  "time"
 )
 
-func secureRandomInt(max int) int {
-	n, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
-	if err != nil {
-		log.Fatalf("Ошибка генерации случайного числа: %v", err)
-	}
-	return int(n.Int64())
-}
-
-func sensorDataGeneratorWithParams(dataChan chan<- int, iterations int, interval time.Duration) {
-	ticker := time.NewTicker(interval)
-	defer ticker.Stop()
-	for i := 0; i < iterations; i++ {
-		dataChan <- secureRandomInt(100)
-		<-ticker.C
-	}
-	close(dataChan)
-}
-
 func sensorDataGenerator(dataChan chan<- int) {
-	sensorDataGeneratorWithParams(dataChan, 120, 500*time.Millisecond)
+  timeout := time.After(time.Minute)
+  ticker := time.NewTicker(500 * time.Millisecond)
+  defer ticker.Stop()
+
+  for {
+    select {
+    case <-timeout:
+      close(dataChan)
+      return
+    case <-ticker.C:
+      value := rand.Intn(100)
+      select {
+      case dataChan <- value:
+      default:
+
+      }
+    }
+  }
 }
 
 func dataProcessor(dataChan <-chan int, processedChan chan<- float64) {
-	batch := make([]int, 0, 10)
-	for value := range dataChan {
-		batch = append(batch, value)
-		if len(batch) == 10 {
-			sum := 0
-			for _, v := range batch {
-				sum += v
-			}
-			processedChan <- float64(sum) / 10.0
-			batch = make([]int, 0, 10)
-		}
-	}
-	close(processedChan)
+  var batch []int
+  for value := range dataChan {
+    batch = append(batch, value)
+    if len(batch) == 10 {
+      sum := 0
+      for _, v := range batch {
+        sum += v
+      }
+      processedChan <- float64(sum) / 10.0
+      batch = nil
+    }
+  }
+  close(processedChan)
 }
 
 func main() {
-	sensorChan := make(chan int)
-	processedChan := make(chan float64)
+  rand.Seed(time.Now().UnixNano())
 
-	var wg sync.WaitGroup
-	wg.Add(2)
+  sensorChan := make(chan int, 10)
+  processedChan := make(chan float64)
 
-	go func() {
-		sensorDataGenerator(sensorChan)
-		wg.Done()
-	}()
+  var wg sync.WaitGroup
+  wg.Add(2)
 
-	go func() {
-		dataProcessor(sensorChan, processedChan)
-		wg.Done()
-	}()
+  go func() {
+    sensorDataGenerator(sensorChan)
+    wg.Done()
+  }()
 
-	go func() {
-		for avg := range processedChan {
-			fmt.Println("Среднее значение:", avg)
-		}
-	}()
+  go func() {
+    dataProcessor(sensorChan, processedChan)
+    wg.Done()
+  }()
 
-	wg.Wait()
+  go func() {
+    for avg := range processedChan {
+      fmt.Println("Среднее значение:", avg)
+    }
+  }()
+  fmt.Print("Ждём")
+  wg.Wait()
 }
